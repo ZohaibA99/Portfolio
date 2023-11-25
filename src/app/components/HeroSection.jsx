8519d74081996033c70a390121742df6e580cf93
ZohaibA99 <118318498+ZohaibA99@users.noreply.github.com>
2023-11-25 13:44:08 -0500

    updated gradient color

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-200 to-blue-600">
               Hello, I&apos;m{" "}
             </span>
             <br></br>
@@ -24,7 +24,9 @@ const HeroSection = () => {
               sequence={[
                 "Zohaib",
                 1000,
-                "a Web/Mobile App Developer",
+                "Web Developer",
+                1000,
+                "Mobile Developer",
                 1000,
                 "Software Engineer",
                 1000,
@@ -35,19 +37,18 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptuous.
+           I create modern, visually stunning, and user-friendly web and mobile applications.
           </p>
           <div>
             <Link
               href="/#contact"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-cyan-200 to-blue-600 hover:bg-transparent text-white"
             >
               Contact
             </Link>
             <Link
               href="/"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-cyan-200 to-blue-600 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
