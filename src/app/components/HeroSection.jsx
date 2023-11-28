63f50bae5cc8b1a3b65b689a7796fc4dcc3dcb70
ZohaibA99 <118318498+ZohaibA99@users.noreply.github.com>
2023-11-28 12:33:14 -0500

    updated site

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -17,7 +17,7 @@ const HeroSection = () => {
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-200 to-blue-600">
-              Hello, I&apos;m{" "}
+              Hello, I&apos;m{" "} a
             </span>
             <br></br>
             <TypeAnimation
